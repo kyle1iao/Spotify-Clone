4f1d5da3b9723647484fa741419829503b1b0e96
Kyle Liao <kyleliao@sas.upenn.edu>
2023-06-24 16:58:02 -0400

    Setup supabase, start creating modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,20 +10,21 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         type={type}
         className={twMerge(
           `
-          w-full
-          rounded-full
+          w-full 
+          rounded-full 
           bg-green-500
           border
           border-transparent
-          px-3
-          py-3
-          disabled:cursor-not-allowed
+          px-3 
+          py-3 
+          disabled:cursor-not-allowed 
           disabled:opacity-50
           text-black
           font-bold
           hover:opacity-75
           transition
           `,
+          disabled && "opacity-75 cursor-not-allowed",
           className
         )}
         disabled={disabled}
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
   image: string;
@@ -18,27 +19,47 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   return (
     <div>
       <button
+        onClick={onClick}
         className="
-    relative
-    group
-    flex
-    items-center
-    rounded-md
-    overflow-hidden
-    gap-x-4
-    bg-neutral-100/10
-    hover:bg-neutral-100/20
-    transition
-    pr-4
+        relative 
+        group 
+        flex 
+        items-center 
+        rounded-md 
+        overflow-hidden 
+        gap-x-4 
+        bg-neutral-100/10 
+        cursor-pointer 
+        hover:bg-neutral-100/20 
+        transition 
+        pr-4
+        min-w-[250px]
     "
       >
         <div
           className="
-        relative
-        min-h-[64px]
-        min-w-[64px]"
+          relative min-h-[64px] min-w-[64px]"
         >
           <Image className="object-cover" fill src={image} alt="Image" />
+        </div>
+        <p className="font-medium truncate py-5">{name}</p>
+        <div
+          className="
+          absolute 
+          transition 
+          opacity-0 
+          rounded-full 
+          flex 
+          items-center 
+          justify-center 
+          bg-green-500 
+          p-4 
+          drop-shadow-md 
+          right-5
+          group-hover:opacity-100 
+          hover:scale-110"
+        >
+          <FaPlay className="text-black" />
         </div>
       </button>
     </div>
