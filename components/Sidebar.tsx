10e6cb4e430195568e84406e72b1c93d0345a076
Kyle Liao <kyleliao@sas.upenn.edu>
2023-06-23 23:09:14 -0400

    Add library

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,32 +6,37 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({children}) => {
-    const pathname = usePathname();
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
 
-    const routes = useMemo(() => [
-        {
-            icon: HiHome,
-            label: "Home",
-            active: pathname !== '/search',
-            href: '/',
-        },
-        {
-            icon: BiSearch,
-            label: 'Search',
-            active: pathname === '/search',
-            href: '/search',
-        }
-    ], [pathname]);
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+      },
+    ],
+    [pathname]
+  );
 
-    return (  
-        <div className='flex h-full'>
-            <div className="
+  return (
+    <div className="flex h-full">
+      <div
+        className="
                 hidden 
                 md:flex 
                 flex-col 
@@ -41,31 +46,29 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
                 w-[300px] 
                 p-2
                 "
-            >
-                <Box>
-                    <div
-                        className="
+      >
+        <Box>
+          <div
+            className="
                         flex
                         flex-col
                         gap-y-4
                         px-5
                         py-4
                         "
-                    >
-                        {routes.map((item) => (
-                            <SidebarItem
-                            key={item.label}
-                            {...item}
-                            />
-                        ))}
-                    </div>
-                </Box>
-                <Box className="overflow-y-auto h-full">
-                    Song Library
-                </Box>
-            </div>
-        </div>
-    );
-}
- 
+          >
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+
 export default Sidebar;
