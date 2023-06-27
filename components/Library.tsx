aa2ce8142d9144398e0c89ab8c24943dc06ef647
Kyle Liao <kyleliao@sas.upenn.edu>
2023-06-26 20:32:54 -0400

    Create UI/backend for song upload

--- components/Library.tsx
+++ components/Library.tsx
@@ -6,8 +6,14 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const { user, subscription } = useUser();
   const uploadModal = useUploadModal();
@@ -70,7 +76,9 @@ const Library = () => {
         px-3
       "
       >
-        List of Songs!
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
       </div>
     </div>
   );
